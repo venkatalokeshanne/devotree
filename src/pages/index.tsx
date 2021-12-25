6070bc38339af54e365cb9c2758cb53b621bf546
Venkata <venkata@artysium.com>
2021-12-25 01:01:24 +0100

    Bug fix home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ function Home() {
         <title>Devotree</title>
       </Head>
     <Image src={coming} alt='Coming soon'/>
-    <h2>Devo&apos;tree&apos; is growing soon...</h2>
+    <h2 className='text-center'>Devo&apos;tree&apos; is growing soon...</h2>
     <p className='text-center'>Meanwhile know about who is creating this tree <a href='/about' className='purple'> Venkata Lokesh Anne </a></p>
     </div>
   )
