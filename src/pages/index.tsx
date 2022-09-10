aa62b2d181030e5a9a9bbdb7f4c5ed514bbb2cdf
Venkata Lokesh <38438146+venkatalokeshanne@users.noreply.github.com>
2022-09-11 00:02:50 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ function Devotree() {
         </script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', 'UA-216111271-1');
