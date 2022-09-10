df095d15b9fa38327ca33caa9dd06e3c8ef5c85d
Venkata Lokesh <38438146+venkatalokeshanne@users.noreply.github.com>
2022-09-10 23:58:27 +0200

    Gtag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,16 @@ function Devotree() {
         <meta name="author" content="Venkata Lokesh Anne"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta name="google-site-verification" content="Ta8veGgn9iC9_zrWH0pjvk8FleL36grNBqtiXrv-VhY" />
+      <!-- Google tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-216111271-1">
+        </script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-216111271-1');
+        </script>
     </Head>
     <Home />
     </>
